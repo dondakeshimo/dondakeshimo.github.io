0f0a8fbbe9c9bf4ee39253f2645725ce92a697c8
dondakeshimo <went.went.takkun135@gmail.com>
2021-09-18 13:57:30 +0900

    chore: add auth, preview

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,8 @@ import '../styles/index.scss'
 
 const tagManagerArgs = {
   gtmId: `${process.env.GTM_ID}`,
+  auth: `${process.env.GTM_AUTH}`,
+  preview: `${process.env.GTM_PREVIEW}`,
   dataLayerName: 'dataLayer',
   dataLayer: {
     platform: 'nextjs',
